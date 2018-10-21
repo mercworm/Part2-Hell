b6893e543397faa8005f8848302ff0ff4b589f3a
Unknown <1008887@student.sae.edu.au>
2018-10-21 15:19:40 +1000

    Added Epilepsy Warning

---
+++ Assets/StartGame.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartGame : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(StartGameThing());	
+	}
+
+    public IEnumerator StartGameThing ()
+    {
+        yield return new WaitForSeconds(3);
+        UnityEngine.SceneManagement.SceneManager.LoadScene("IntroScene");
+    }
+}
